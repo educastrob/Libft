d012ea860cfede60d31ec52a030679f688dab688
eduardo <eduardocastrobritoo@gmail.com>
2023-06-06 19:16:20 -0300

    add strjoin and fix bugs (norminette)

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,35 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eduardo <eduardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/06 18:42:49 by eduardo           #+#    #+#             */
-/*   Updated: 2023/06/06 19:03:29 by eduardo          ###   ########.fr       */
+/*   Created: 2023/06/06 18:57:04 by eduardo           #+#    #+#             */
+/*   Updated: 2023/06/06 19:14:34 by eduardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
 	size_t	i;
 	size_t	j;
 
-	str = (char *) ft_calloc(sizeof(*s), len + 1);
+	str = (char *) ft_calloc(sizeof(*s1), ft_strlen(s1) + ft_strlen(s2) + 1);
 	i = 0;
 	j = 0;
 	if (!str)
 		return (NULL);
-	while (s[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+		str[j++] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str[j++] = s2[i];
 		i++;
 	}
 	str[j] = '\0';
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eduardo <eduardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:52:52 by edcastro          #+#    #+#             */
-/*   Updated: 2023/06/06 18:51:34 by eduardo          ###   ########.fr       */
+/*   Updated: 2023/06/06 19:00:36 by eduardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char    *ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
 
 #endif
